5965f2cc277a0fc7f61b1516c7deb895f6b71fb0
timmit147 <timmeeuwsen@hotmail.nl>
2021-04-08 20:27:00 +0200

    go in folder bug addeventlister

--- modules/inception.js
+++ modules/inception.js
@@ -1,18 +1,16 @@
-
-getList();
-if (list["items"] == null){
-
-list["route"] = "items";
-list["items"] = {};
+if(localStorage.getItem("savedData") != null ){
+	getList();
 }
-addLocalStorage()
-getList();
+
+else{
+	localStorage.setItem("savedData", JSON.stringify(list));
+	list["route"] = "items";
+	list["items"] = {};
+	addLocalStorage();
+	getList();
+}
 
 var route = list["route"];
-
-console.log(list);
-
-localStorage.setItem("savedData", JSON.stringify(list));
 
 function getList(){
 	list = JSON.parse(localStorage.getItem("savedData"));
@@ -38,12 +36,12 @@ Object.byString = function(o, s) {
     }
     return o;
 }
-
+inception();
 
 function inception(){
 	if(document.getElementById("home")){
 		getList();
-		for (item in Object.byString(list, route)) {
+		for (item in Object.byString(list, list["route"])) {
 			if(item != null){
 			  document.getElementById('log').innerHTML += "<section id= '" + item + "'> <p>" + item + "</p> </section>"
 			}		
@@ -51,67 +49,95 @@ function inception(){
 	}
 	const box = document.querySelectorAll("#log section");
 
-	for (let i = 0; i < box.length; i++) {
+		for (let i = 0; i < box.length; i++) {
+
+			// wanneer ik klick op section do funtie 
 			box[i].addEventListener("click", function() {
-			// console.log(box[i]);
-			box[i].classList.toggle("active");
 
-			if (box[i].classList.contains("active")){
-				document.querySelector("#folder-icon").style.display = "unset";
-				document.querySelector("#slider-icon").style.display = "none";
-				document.querySelector("#plus-icon").style.display = "none";
-				document.querySelector("#trash-icon").style.display = "unset";
-				document.querySelector("#pen-icon").style.display = "unset";
-				// document.querySelector("#home-back-icon").style.display = "unset";
-				box[i].style.textDecoration  = "none";
-			}
-
-			else{
-				document.querySelector("#folder-icon").style.display = "none";
-				document.querySelector("#slider-icon").style.display = "unset";
-				document.querySelector("#plus-icon").style.display = "unset";
-				document.querySelector("#trash-icon").style.display = "none";
-				document.querySelector("#pen-icon").style.display = "none";
-				// document.querySelector("#home-back-icon").style.display = "none";
-				box[i].style.textDecoration  = "unset";
-			}
-			document.getElementById("trash-icon").onclick = function() { remove(box[i].id) };
-
-		document.getElementById("pen-icon").onclick = function() { change(box[i].id) };
-
-			document.querySelector("#folder-icon").addEventListener("click", function() {
-				document.querySelector("#folder-icon").style.display = "none";
-				document.querySelector("#slider-icon").style.display = "unset";
-				document.querySelector("#plus-icon").style.display = "unset";
-				document.querySelector("#trash-icon").style.display = "none";
-				document.querySelector("#pen-icon").style.display = "none";
-				box[i].style.textDecoration  = "unset";
-					document.getElementById('log').innerHTML = "";
-					// var route = route+' def'; 
-					list["route"] = route + "." + box[i].id;
-					route = list["route"] ;
-					console.log(list["route"] ); 
-					addLocalStorage();
-					for (item in Object.byString(list, route)) {
-						inception();
-					}
+				// wanneer section is active alert
+				if (box[i].classList.contains("active")){
+					box[i].classList.remove("active");
 					document.querySelector("#folder-icon").style.display = "none";
-				
-			});
+					document.querySelector("#slider-icon").style.display = "unset";
+					document.querySelector("#plus-icon").style.display = "unset";
+					document.querySelector("#trash-icon").style.display = "none";
+					document.querySelector("#pen-icon").style.display = "none";
+					box[i].style.textDecoration  = "none";
+				}
+				else{
+					for (let i = 0; i < box.length; i++) {
+						if (box[i].classList.contains("active")){
+							box[i].classList.toggle("active");
+							box[i].style.textDecoration  = "none";
+						}
+					}
+
+					box[i].classList.toggle("active");
+
+
+
+					if (box[i].classList.contains("active")){
+						document.querySelector("#folder-icon").style.display = "unset";
+						document.querySelector("#slider-icon").style.display = "none";
+						document.querySelector("#plus-icon").style.display = "none";
+						document.querySelector("#trash-icon").style.display = "unset";
+						document.querySelector("#pen-icon").style.display = "unset";
+						box[i].style.textDecoration  = "underline";
+					}
+
+					else{
+						document.querySelector("#folder-icon").style.display = "none";
+						document.querySelector("#slider-icon").style.display = "unset";
+						document.querySelector("#plus-icon").style.display = "unset";
+						document.querySelector("#trash-icon").style.display = "none";
+						document.querySelector("#pen-icon").style.display = "none";
+						box[i].style.textDecoration  = "none";
+					}
+					
+					document.querySelector("#folder-icon").addEventListener("click", function() {
+						document.querySelector("#folder-icon").style.display = "none";
+						document.querySelector("#slider-icon").style.display = "unset";
+						document.querySelector("#plus-icon").style.display = "unset";
+						document.querySelector("#trash-icon").style.display = "none";
+						document.querySelector("#pen-icon").style.display = "none";
+						document.querySelector("#home-back-icon").style.display = "unset";
+						getList();
+						list["route"] = list["route"] + "." + box[i].id;
+						console.log(list["route"] ); 
+						addLocalStorage();
+
+						document.getElementById('log').innerHTML = "";
+						inception();
+
+					}, {once : true});
+
+					document.getElementById("trash-icon").onclick = function() { remove(box[i].id) };
+
+					document.getElementById("pen-icon").onclick = function() { change(box[i].id) };
+
+				}
 			
-		});
+			});
+		}
+}
+
+if(document.querySelector("#home-back-icon")){
+	if(list["route"] == "items"){
+		document.querySelector("#home-back-icon").style.display = "none";
 	}
 }
 
 if(document.querySelector("#home-back-icon")){
 	document.querySelector("#home-back-icon").addEventListener("click", function() {
-		list["route"] = "items";
+		var str = list["route"];
+	var lastIndex = str.lastIndexOf(".");
+	str = str.substring(0, lastIndex);
+		list["route"] = str;
 		addLocalStorage();
 		location.reload();
 	});
-	}
+}
 
-inception();
 
 
 // activate on save page
@@ -133,25 +159,16 @@ if(document.getElementById("save")){
 }
 
 
-
 // activate funtion after click
 function remove(x){
-
-
-getList();
-
-
-console.log(Object.byString(list, list["route"] )[x]);
-delete Object.byString(list, list["route"] )[x];
-console.log(Object.byString(list, list["route"] )[x]);
-
-addLocalStorage();
-location.reload();
-
+	getList();
+	delete Object.byString(list, list["route"] )[x];
+	addLocalStorage();
+	location.reload();
 }
 
 function change(x){
-window.location.href = "edit.html?item=" + x;
+	window.location.href = "edit.html?item=" + x;
 }
 
 
